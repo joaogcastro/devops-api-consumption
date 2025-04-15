bf4ec9fe5e19250572505d9119393abf47815b00
Joao Castro <joao.castro@atos.net>
2025-04-15 20:36:48 -0300

    ejs atualizado

--- client/src/index.js
+++ client/src/index.js
@@ -8,24 +8,21 @@ const PORT = 4000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Dados estáticos que podem ser utilizados no template
 const dadosEstaticos = {
     titulo: 'Viajar Ponto Com',
-    mensagem: 'Escolha seu voo!',
-    itens: ['Item 1', 'Item 2', 'Item 3'],
+    mensagem: 'Escolha seu voo!'
 };
 
 app.get('/', (req, res) => {
     axios.get("http://localhost:3000/voo")
     .then(response => {
-        const dadosVoo = response.data; // Acesse os dados da resposta
-        console.log(JSON.stringify(dadosVoo)); // Logar os dados recebidos
-        // Passar dados estáticos e os dados do voo para o template
+        const dadosVoo = response.data;
+        // console.log(JSON.stringify(dadosVoo)); 
         res.render('index', { ...dadosEstaticos, dados: dadosVoo });
     })
     .catch(error => {
-        console.error("Erro ao buscar dados:", error); // Logar o erro
-        res.status(500).send("Erro ao buscar dados"); // Responder com erro
+        console.error("Erro ao buscar dados:", error);
+        res.status(500).send("Erro ao buscar dados");
     });
 });
 
